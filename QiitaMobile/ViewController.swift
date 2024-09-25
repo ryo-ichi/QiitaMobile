85877599498cc4081495a379fccea8ab747dbeab
松原涼一 <>
2024-09-25 20:34:53 +0900

    xcode雛形作成完了

---
+++ QiitaMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QiitaMobile
+//
+//  Created by 松原涼一 on 2024/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
