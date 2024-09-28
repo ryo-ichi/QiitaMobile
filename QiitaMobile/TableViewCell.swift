95cb7f3fe60357fbca942b71064c26e6e80e374b
松原涼一 <>
2024-09-28 15:35:51 +0900

    セルファイルとXIBファイル追加

---
+++ QiitaMobile/TableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewCell.swift
+//  QiitaMobile
+//
+//  Created by 松原涼一 on 2024/09/28.
+//
+
+import UIKit
+
+class TableViewCell: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
