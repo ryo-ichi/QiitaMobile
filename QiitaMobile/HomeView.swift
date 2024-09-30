dd0653df0ca6719f8ef1407c4a8c3d7941356926
松原涼一 <>
2024-09-30 18:43:28 +0900

    クラス名及びファイル名再変更

--- QiitaMobile/HomeView.swift
+++ QiitaMobile/HomeView.swift
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class TabBarController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
 }
 
